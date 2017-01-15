0b63e7ecb1b54c6cb7c221ea0a2b2b094c5a4fb7
ferchee <ferenc.nyerges@gmail.com>
2017-01-15 14:12:02 +0100

    First change in TestFeatures-1

--- ConsoleTest/ConsoleTest/Program.cs
+++ ConsoleTest/ConsoleTest/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleTest
         static void Main(string[] args)
         {            
             WriteMe("Hello");
+            Console.WriteLine("New feature");
             Console.ReadLine();
         }
 
